0b99e8ee9b633c82e12e92c3ec8f7a4007ae5332
DalindaLimon <102000093+DalindaLimon@users.noreply.github.com>
2023-10-30 15:57:29 -0400

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,9 +1,2 @@
-local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent('hunger', function(src,args)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local newHunger = Player.PlayerData.metadata['hunger']
-    return newHunger
-end)
     
